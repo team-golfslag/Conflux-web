f3f047645611185b94350c5408fed7818716a19f
Tijmen Vis <tijmenpvis@gmail.com>
2025-02-26 16:49:51 +0100

    feat CX-47: footer middle?

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -1,7 +1,21 @@
 import 'react';
 
 function footer() {
-    return <div className="text-orange-500">yoo</div>;
+    return (
+    <div className="text-orange-500 bottom-10">
+        {links(["hello","jieaf"])}
+        <text>Created by Team Golfslag</text>
+    </div>);
+}
+
+function links(texts: string[]) {
+    // will be a list of objects with text, icon, link
+    const formattedTexts = texts.map(text => 
+    (<li>
+        <a href="http://www.google.com">{text}</a>
+    </li>)
+    );
+    return <ul>{formattedTexts}</ul>;
 }
 
 export default footer;
