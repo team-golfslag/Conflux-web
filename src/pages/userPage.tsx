44cb181a70bdb5d8296810a0f1973d7fd8641a62
lukeksnceuoibceiofegofiqw <lukeheeftmail@gmail.com>
2025-02-27 13:45:30 +0100

    feat/CX-52-skeleton-user-pagina

---
+++ src/pages/userPage.tsx
@@ -0,0 +1,28 @@
+/*
+  This program has been developed by students from the bachelor Computer Science at Utrecht
+  University within the Software Project course.
+
+  © Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
+
+import { Link } from "react-router"
+
+/*
+this is the main entry point into the projects page. 
+It is currently empty.
+*/
+
+function UserPage() 
+{
+    return (
+    <>
+        <p>name</p>
+        <p>mail</p>
+        <p>get contact info</p>
+        <Link to = "/"> go to home </Link>
+    </>
+    )
+}
+
+export default UserPage
