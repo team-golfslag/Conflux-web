735090c4d77ebbfeb6d97c83cf300f582ecc779f
StefanHerald <94446521+StefanHerald@users.noreply.github.com>
2025-03-05 12:12:37 +0100

    feat: added new components and fonts
    I added a new components and fonts folder. Of this, I have created 2 new buttons and a (potential) text block for how projects are supposed to look. Also added one regular variant of both fonts.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -8,28 +8,39 @@
 
 import { useState } from 'react'
 import { Link } from 'react-router'
-import './App.css'
+import { primaryButton, secondaryButton } from '../Components/buttons'
+import { projectTextBlock } from '../Components/textBlockElements'
 
 /*
 This is a (temporary) file which serves as that which is seen at home. It has links to the other pages.
 ./App.css may give an error, but it should just work. Ask Max to intimidate the computer for you
 */
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count, setCount] = useState(0) 
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className='flex justify-center pt-50'>
+      <div className='flex items-center gap-10 flex-col'>
+        <h1 >Vite + React</h1>
+          {projectTextBlock("On the weasles with the measles", "This project gives a comprehesive overview on how the measles virus has spread throughout weasles due to the barbie movie. In this essay I will...")}
+        <div className="flex justify-normal flex-row">
+          {primaryButton("Count is " + count, () => {setCount(count + 1)} )}
+          {primaryButton("Another button!")}
+          {primaryButton("This increments!", () => {setCount(count + 1)} )}
+        </div>
+        <div>
+          {secondaryButton("Button!", () => {setCount(count + 1)} )}
+        </div>
+        <Link to="/projectsPage" >Go to projectsPage </Link>
+        <hr/>
+        <Link to="/settings" >Go to settings </Link>
       </div>
-      <Link to="/projectsPage" >Go to projectsPage </Link>
-      <hr/>
-      <Link to="/settings" >Go to settings </Link>
-    </>
+    </div>
+    
   )
 }
 
