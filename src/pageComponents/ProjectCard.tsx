e0f5b1d33e8e949067c7071f66f71eb4df59c65c
PMM <maxmaes2004@gmail.com>
2025-03-13 12:35:02 +0100

    Feat: added styling to ProjectCard.tsx zodat het meer lijkt zoals design

--- src/pageComponents/ProjectCard.tsx
+++ src/pageComponents/ProjectCard.tsx
@@ -6,9 +6,16 @@ type ProjectCardProps = {
 
 
 const ProjectCard = ({project}: ProjectCardProps) => {
-    return <div className="bg-white w-screen">
-        <h1>{project.title}</h1>
-    </div>
+    return <div className="bg-white max-w-4xl rounded border-2 border-gray-400 px-4 py-2 flex row space-between ">
+                <div className="mx-4 max-w-xl">
+                    <h1>{project.title}</h1>
+                    <p>{project.description}</p>
+                </div>
+                <div className="mx-4">
+                    <p>start date: {project.startDate?.toDateString()}</p>
+                    <p>end date: {project.endDate?.toDateString()}</p>
+                </div>
+            </div>
 }
 
 export default ProjectCard
