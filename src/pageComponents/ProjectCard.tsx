08577af277b3b1c2e716abf4d5ab511be0ec62b1
PMM <maxmaes2004@gmail.com>
2025-03-15 21:20:14 +0100

    feat: Added image formatting for projectcards and fixed some visual things on the search page. Currently the image is hardcoded but this can be easily changed once we decide how to store project images.

--- src/pageComponents/ProjectCard.tsx
+++ src/pageComponents/ProjectCard.tsx
@@ -4,16 +4,21 @@ type ProjectCardProps = {
     project: Project
 }
 
-
+const truncater = (str: string, length: number) => {
+    return str.substring(0, Math.min(str.length, length)) + "...";
+}
 const ProjectCard = ({project}: ProjectCardProps) => {
-    return <div className="bg-white max-w-4xl rounded border-2 border-gray-400 px-4 py-2 flex row space-between ">
-                <div className="mx-4 max-w-xl">
-                    <h1>{project.title}</h1>
-                    <p>{project.description}</p>
+    return <div className="bg-white max-w-5xl rounded border-2 border-gray-400 px-4 py-2 flex">
+                <div className="w-1/6">
+                    <img className="rounded-full" src="/src/assets/Golfslag.png" alt="team photo"/>
+                </div>
+        <div className="mx-4 w-3/5">
+            <h1>{project.title}</h1>
+                    <p className="text-base">{truncater(project.description as string, 350)}</p>
                 </div>
                 <div className="mx-4">
-                    <p>start date: {project.startDate?.toDateString()}</p>
-                    <p>end date: {project.endDate?.toDateString()}</p>
+                    <p className="text-sm">start date: {project.startDate?.toDateString()}</p>
+                    <p className="text-sm">end date: {project.endDate?.toDateString()}</p>
                 </div>
             </div>
 }
