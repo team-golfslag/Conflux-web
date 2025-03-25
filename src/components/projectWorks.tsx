bc2189ba08dfdcc9af58f606a5b926b232042a1d
stefanHerald <s.herald@students.uu.nl>
2025-03-25 16:25:21 +0100

    feat: added react-async and made clickable text
    I added react-async in order to watch fetches and see which links should be clickable after the fetches have resolved.

--- src/components/projectWorks.tsx
+++ src/components/projectWorks.tsx
@@ -7,24 +7,54 @@
 
 import {Card, CardContent} from "@/components/ui/card";
 import {Product} from "@/types/models.ts";
+import { AsyncState, useAsync } from "react-async"
 
 type ProjectWorksProps = { products: Product[] }
+
+/**
+ * an async function which fetches the head of an url and return if the response was ok
+ * @param {string} url
+ * @returns {bool}
+ */
+
+const isValidLink = async (url : string) => {
+    try{
+      const response = await fetch(url, {method : "HEAD"}); //do a fetch without requiring full body data. only retrieve HEAD for status.
+      return response.ok;
+    }catch{
+      return false; //url was false 
+    }
+}
 
 /**
  * Project Works component
  * @param props the products to be turned into a card
  */
 export default function ProjectWorks(props: ProjectWorksProps) {
+
+    const validUrlArray : AsyncState<boolean>[] = []
+    for(let i = 0 ; i < props.products.length; i++){
+        const asyncObject : AsyncState<boolean> = useAsync(isValidLink(props.products[i].url));
+        validUrlArray.push(asyncObject);
+    }
+
     return (
         <section className="space-y-4 bg-white p-6 shadow rounded-lg">
             <h2 className="text-xl font-semibold mb-4">Works</h2>
-            {props.products.map((product) => (
+            {props.products.map((product, index) => (
+                
                 <Card key={product.id} className="bg-gray-200">
                     <CardContent className="flex items-center gap-4 p-3">
                         <div>
                             <p className="font-semibold">{product.title}</p>
                             <p className="text-sm text-gray-600">Details</p>
-                            <p className="text-sm text-gray-600">{product.url}</p>
+                            {validUrlArray[index].isResolved && validUrlArray[index].data ? 
+                                <a target= "_blank" 
+                                   href={product.url} 
+                                   className="text-sm text-blue-600 hover:underline focus:text-purple-700">
+                                    {product.url}</a> : 
+                                <p className="text-sm text-gray-600">{product.url}</p>
+                            }
                         </div>
                     </CardContent>
                 </Card>
