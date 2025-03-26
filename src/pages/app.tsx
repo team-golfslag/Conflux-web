573065423ba45cf6b23db760aec7e7fb5f47084d
PMM <maxmaes2004@gmail.com>
2025-03-26 10:47:14 +0100

    feat: Added landing page referring to a (currently) non-existent login route so instead it takes you to the search page.

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -4,13 +4,26 @@
 
   © Copyright Utrecht University (Department of Information and Computing Sciences)
 */
+import {Button} from "@/components/ui/button.tsx";
 
-import {Navigate} from "react-router";
 
 function App() {
-    return (
-        <Navigate to="/projects/search"/>
-    )
+    return <>
+        <div className="w-screen h-screen bg-background flex flex-col items-center justify-center">
+            <div className="m-5 transition-opacity duration-2000 opacity-100 flex flex-col items-center">
+                <h1 className="text-6xl">Welcome to Conflux</h1>
+                <p className="">We kindly ask you to login for a more personalized experience</p>
+            </div>
+            <div className = "transition-opacity duration-3000 opacity-100">
+                <Button onClick = {validation}>Log in</Button>
+            </div>
+        </div>
+
+    </>
+
 }
+
+const validation = () => window.location.href= '/projects/search'
+
 
 export default App
