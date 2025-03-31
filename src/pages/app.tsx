748526e0ba68e2152ff769def019a2b0ddb9c0a0
PMM <maxmaes2004@gmail.com>
2025-03-31 13:15:50 +0200

    refactor: Removed unnecessary classes

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -14,11 +14,11 @@ function App() {
                 <img className="rounded-full w-30 h-30 object-contain" src="/src/assets/golfslag.png"
                      alt="Golfslag logo"/>
             </div>
-            <div className="m-5 transition-opacity duration-2000 opacity-100 flex flex-col items-center">
+            <div className="m-5 flex flex-col items-center">
                 <h1 className="text-6xl">Welcome to Conflux</h1>
                 <p className="">We kindly ask you to login for a more personalized experience</p>
             </div>
-            <div className = "transition-opacity duration-3000 opacity-100">
+            <div className = "">
                 <Button onClick = {validation}>Log in</Button>
             </div>
 
