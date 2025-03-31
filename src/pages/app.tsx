f239aaa22362a26d71bded4381f6e1a29ef83eeb
PMM <maxmaes2004@gmail.com>
2025-03-31 13:23:21 +0200

    refactor: Removed unnecessary decorators

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -10,15 +10,15 @@ import {Button} from "@/components/ui/button.tsx";
 function App() {
     return <>
         <div className="w-screen h-screen bg-background flex flex-col items-center justify-center">
-            <div className = "">
+            <div>
                 <img className="rounded-full w-30 h-30 object-contain" src="/src/assets/golfslag.png"
                      alt="Golfslag logo"/>
             </div>
             <div className="m-5 flex flex-col items-center">
                 <h1 className="text-6xl">Welcome to Conflux</h1>
-                <p className="">We kindly ask you to login for a more personalized experience</p>
+                <p>We kindly ask you to login for a more personalized experience</p>
             </div>
-            <div className = "">
+            <div className>
                 <Button onClick = {validation}>Log in</Button>
             </div>
 
