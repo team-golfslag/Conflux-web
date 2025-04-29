ebb78e792ccad121258691fdfbe99ff18a22e72d
PMM <maxmaes2004@gmail.com>
2025-04-29 11:22:51 +0200

    refactor: Styling in file improved

--- src/components/contributorSelect.tsx
+++ src/components/contributorSelect.tsx
@@ -4,47 +4,73 @@
  * © Copyright Utrecht University (Department of Information and Computing Sciences)
  */
 
-import {ApiClient, Contributor} from "@team-golfslag/conflux-api-client/src/client";
-import {ApiClientContext} from "@/lib/ApiClientContext.ts";
-import {useContext, useEffect, useState} from "react";
-import {Input} from "@/components/ui/input.tsx";
+import {
+  ApiClient,
+  Contributor,
+} from "@team-golfslag/conflux-api-client/src/client";
+import { ApiClientContext } from "@/lib/ApiClientContext.ts";
+import { useContext, useEffect, useState } from "react";
+import { Input } from "@/components/ui/input.tsx";
 import ProjectContributors from "@/components/projectContributors.tsx";
-import {Button} from "@/components/ui/button.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
-type ContributorSelectProps = {projectId: string}
+type ContributorSelectProps = { projectId: string };
 
 const ContributorSelect = (props: ContributorSelectProps) => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [contributors, setContributors] = useState<Contributor[]>();
-    const [selectedContributor, setSelectedContributor] = useState<string>();
-    const apiClient = useContext(ApiClientContext);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [contributors, setContributors] = useState<Contributor[]>();
+  const [selectedContributor, setSelectedContributor] = useState<string>();
+  const apiClient = useContext(ApiClientContext);
 
-    useEffect(() => {
-        apiClient.contributors_GetContributorsByQuery(searchTerm).then(result => setContributors(result));
-    }, [searchTerm, apiClient]);
+  useEffect(() => {
+    apiClient
+      .contributors_GetContributorsByQuery(searchTerm)
+      .then((result) => setContributors(result));
+  }, [searchTerm, apiClient]);
 
-    return <>
-        <div className="flex flex-row justify-center py-16">
-            <Input
-                className="w-1/3 rounded-2xl"
-                type="search"
-                placeholder="Input contributor name to search"
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <Button onClick={ () => addContributorToProject(props.projectId, apiClient, selectedContributor)}>Add contributor</Button>
-        </div>
-        <div className="flex flex-col items-center">
-            <h2 className="mb-8 text-3xl font-bold">Results</h2>
-            <ProjectContributors people = {contributors?.slice(0, 5)} onSelect={setSelectedContributor} />
-        </div>
+  return (
+    <>
+      <div className="flex flex-row justify-center py-16">
+        <Input
+          className="w-1/3 rounded-2xl"
+          type="search"
+          placeholder="Input contributor name to search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Button
+          onClick={() =>
+            addContributorToProject(
+              props.projectId,
+              apiClient,
+              selectedContributor,
+            )
+          }
+        >
+          Add contributor
+        </Button>
+      </div>
+      <div className="flex flex-col items-center">
+        <h2 className="mb-8 text-3xl font-bold">Results</h2>
+        <ProjectContributors
+          people={contributors?.slice(0, 5)}
+          onSelect={setSelectedContributor}
+        />
+      </div>
     </>
-}
+  );
+};
 
-const addContributorToProject = (projectId: string, apiClient: ApiClient, contributorId?: string) => {
-    if(contributorId === undefined) {
-        return;
-    }
-    apiClient.projects_AddContributorToProject(projectId, contributorId).then(() => {})
-}
+const addContributorToProject = (
+  projectId: string,
+  apiClient: ApiClient,
+  contributorId?: string,
+) => {
+  if (contributorId === undefined) {
+    return;
+  }
+  apiClient
+    .projects_AddContributorToProject(projectId, contributorId)
+    .then(() => {});
+};
 
 export default ContributorSelect;
--- src/components/projectContributors.tsx
+++ src/components/projectContributors.tsx
@@ -7,21 +7,28 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Contributor } from "@team-golfslag/conflux-api-client/src/client";
 
-type ProjectContributorsProps = { people?: Contributor[], onSelect?: (contributorId: string) => void };
-
+type ProjectContributorsProps = {
+  people?: Contributor[];
+  onSelect?: (contributorId: string) => void;
+};
 
 /**
  * Project Contributors component
  * @param props the people to be turned into a card
  */
 export default function ProjectContributors({
-  people, onSelect,
+  people,
+  onSelect,
 }: Readonly<ProjectContributorsProps>) {
   return (
     <section className="h-full space-y-4 rounded-lg bg-white p-6 shadow">
       <h2 className="mb-4 text-xl font-semibold">Contributors</h2>
       {people?.map((person) => (
-        <Card key={person.id} onClick={() => onSelect?.(person.id)} className="bg-gray-200">
+        <Card
+          key={person.id}
+          onClick={() => onSelect?.(person.id)}
+          className="bg-gray-200"
+        >
           <CardContent className="flex items-center gap-4 p-3">
             <div className="h-12 w-12 rounded-full bg-gray-500" />
             <div>
