528735cb9d91edf873176f28451140804a80d756
Dinandius <dinand.wesdorp@live.nl>
2025-04-29 10:57:38 +0200

    feat: added a list of projects for the dashboard

--- src/components/dashboardListView.tsx
+++ src/components/dashboardListView.tsx
@@ -4,8 +4,9 @@
  * © Copyright Utrecht University (Department of Information and Computing Sciences)
  */
 
-import DashboardCard, { DashboardCardProps } from "@/components/dashboardCard";
+import { DashboardCardProps } from "@/components/dashboardCard";
 import { JSX } from "react";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion.tsx";
 
 interface DashboardListViewProps {
   data?: DashboardCardProps[];
@@ -13,10 +14,26 @@ interface DashboardListViewProps {
 
 const DashboardListView = ({ data }: DashboardListViewProps): JSX.Element => {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {data?.map((cardProps, index) => (
-        <DashboardCard key={index} {...cardProps} />
-      ))}
+    <div className="items-center">
+      <Accordion type="single" collapsible className="w-full">
+        {data?.map((rowProps) => (
+        <AccordionItem value={rowProps.project.id}>
+          <AccordionTrigger>{rowProps.project.title}</AccordionTrigger>
+          <AccordionContent>
+            <div className="flex h-5 items-center space-x-10 ml-3 text-sm">
+                <h3 className="font-semibold">Roles</h3>
+                <p>{rowProps.role}</p>
+            </div>
+              <div className="flex h-5 items-center space-x-4 mt-2 text-sm ml-3">
+                  <h3 className="font-semibold">Start Date</h3>
+                  <p>{rowProps.project.start_date?.toDateString()}</p>
+                  <h3 className="font-semibold">End Date</h3>
+                  <p>{rowProps.project.end_date?.toDateString()}</p>
+              </div>
+          </AccordionContent>
+        </AccordionItem>
+          ))}
+      </Accordion>
     </div>
   );
 };
---
+++ src/components/ui/accordion.tsx
@@ -0,0 +1,62 @@
+/**
+ * This program has been developed by students from the bachelor Computer Science at Utrecht
+ * University within the Software Project course.
+ * © Copyright Utrecht University (Department of Information and Computing Sciences)
+ */
+"use client"
+
+import * as React from "react"
+import * as AccordionPrimitive from "@radix-ui/react-accordion"
+import { ChevronDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+const Accordion = AccordionPrimitive.Root
+
+const AccordionItem = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Item>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
+    <AccordionPrimitive.Item
+        ref={ref}
+        className={cn("border-b hover:border-2 px-2 hover:border-purple-700 hover:shadow-lg hover:shadow-gray-300 duration-100", className)}
+        {...props}
+    />
+))
+AccordionItem.displayName = "AccordionItem"
+
+const AccordionTrigger = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Trigger>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+    <AccordionPrimitive.Header className="flex">
+        <AccordionPrimitive.Trigger
+            ref={ref}
+            className={cn(
+                "flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all text-left [&[data-state=open]>svg]:rotate-180 duration-300",
+                className
+            )}
+            {...props}
+        >
+            {children}
+            <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+        </AccordionPrimitive.Trigger>
+    </AccordionPrimitive.Header>
+))
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
+
+const AccordionContent = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Content>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+    <AccordionPrimitive.Content
+        ref={ref}
+        className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+        {...props}
+    >
+        <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    </AccordionPrimitive.Content>
+))
+AccordionContent.displayName = AccordionPrimitive.Content.displayName
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
