f394611c8703d3745fb898640fcbccb1835b8626
Lucas Jenkins <98951426+lfj76@users.noreply.github.com>
2025-05-06 14:42:39 +0200

    feat: clean up styling & layout contributors

--- src/components/projectContributors.tsx
+++ src/components/projectContributors.tsx
@@ -4,7 +4,7 @@
  * © Copyright Utrecht University (Department of Information and Computing Sciences)
  */
 
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { Contributor } from "@team-golfslag/conflux-api-client/src/client";
 
 type ProjectContributorsProps = { contributors?: Contributor[] };
@@ -17,21 +17,23 @@ export default function ProjectContributors({
   contributors,
 }: Readonly<ProjectContributorsProps>) {
   return (
-    <section className="h-full space-y-4 rounded-lg bg-white p-6 shadow">
-      <h2 className="mb-4 text-xl font-semibold">Contributors</h2>
-      {contributors?.map((contributor) => (
-        <Card key={contributor.id} className="bg-gray-200">
-          <CardContent className="flex items-center gap-4 p-3">
-            <div className="h-12 w-12 rounded-full bg-gray-500" />
+    <>
+      <h2 className="text-xl font-semibold">Contributors</h2>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        {contributors?.map((contributor) => (
+          <Card
+            key={contributor.id}
+            className="flex-row items-center gap-4 border border-gray-200 p-3 shadow-sm"
+          >
             <div>
               <p className="font-semibold">{contributor.name}</p>
               <p className="text-sm text-gray-600">
                 {contributor.roles.map((role) => role.name).join(", ")}
               </p>
             </div>
-          </CardContent>
-        </Card>
-      ))}
-    </section>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
